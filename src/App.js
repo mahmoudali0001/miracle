5ebf9710b933d717a54a42a30b6ae4bbcd497f09
Mahmoud Ali <mahmoudalifarag1@gmail.com>
2023-07-22 20:50:53 +0200

    start project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className="text-3xl text-gray-100 shadow-xl rounded-full bg-cyan-600 w-1/2 m-auto mt-2">
+        My Dream Is live
+      </h3>
+    </div>
+  );
+}
+
+export default App;

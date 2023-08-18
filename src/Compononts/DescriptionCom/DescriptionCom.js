73c31bdaa2bff86e5ac796f0f1768ba4f9bd9f70
ZEXAWY <freedomblackknights@gmail.com>
2023-08-18 22:21:02 +0300

    change

--- src/Compononts/DescriptionCom/DescriptionCom.js
+++ src/Compononts/DescriptionCom/DescriptionCom.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const descriptionCom = (props) => {
   return (
-    <div className='text-right mx-[10px] text-black pb-2 '>
+    <div className='flex flex-col justify-end items-end mx-[10px] text-black pb-2 '>
       <h3 className='font-bold text-2xl'>Description</h3>
-      <p className='font-light'>
+      <p className='font-light w-[40%]'>
         The product info will be displayed in here in some real time case
         without the need of anyone having multiple reason for the case in the
         mean time i will have them all here and to attach this we coulg replace

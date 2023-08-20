58688c2cba939ea6143beaaa65d5914065ff1de2
ZEXAWY <freedomblackknights@gmail.com>
2023-08-20 06:30:36 +0300

    removing uneccasry whitespaces

--- src/Compononts/TextCom/TextCom.js
+++ src/Compononts/TextCom/TextCom.js
@@ -3,8 +3,7 @@ const textCom = (props) => {
   const firstLineClass = 'font-light';
   const secondLineClass = 'font-bold text-2xl';
   return (
-    <div
-      className={'rounded-xl flex flex-col justify-between bg-gradient-to-r  '}>
+    <div className={'rounded-xl flex flex-col justify-between'}>
       <span className={[firstLineClass, props.className1].join(' ')}>
         {props.firstLine ? props.firstLine : 'Enjoy'}
       </span>

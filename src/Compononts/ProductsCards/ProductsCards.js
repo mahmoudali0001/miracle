81293a2252a3b1ba0f54b59e5bd4b4455c244cc7
Mahmoud Ali <mahmoudalifarag1@gmail.com>
2023-08-24 21:42:38 +0200

    change class name

--- src/Compononts/ProductsCards/ProductsCards.js
+++ src/Compononts/ProductsCards/ProductsCards.js
@@ -5,14 +5,19 @@ import ProductsCard from "./ProductsCard/ProductsCard";
 const ProductsCards = (props) => (
   <section className="grid md:grid-cols-4 grid-cols-2 gap-3 mx-2">
     <ProductsCard styleClass="md:col-span-1 col-span-2 bg-linear-black text-white" />
-    <ProductsCard styleClass="kkkk bg-linear-yellow text-white" />
+
+    <ProductsCard styleClass="productsCard bg-linear-yellow text-white" />
+
     <ProductsCard styleClass="md:col-span-2 col-span-1 bg-linear-red text-white" />
+
     <ProductsCard
       styleClass="md:col-span-2 col-span-2 bg-linear-gray"
       spanTxtClass="text-white"
     />
-    <ProductsCard styleClass="kkkk bg-linear-green text-white" />
-    <ProductsCard styleClass="kkkk bg-linear-blue text-white" />
+
+    <ProductsCard styleClass="productsCard bg-linear-green text-white" />
+
+    <ProductsCard styleClass="productsCard bg-linear-blue text-white" />
   </section>
 );
 
